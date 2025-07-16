c4515929d2d60d5f5a31fd8b5496e5c6cbb3bde7
Theophilus Danladi Umar <theophilusumar27@gmail.com>
2025-07-16 07:10:13 +0100

    progress on adding some functionalities

--- OLD TASK/Assignments/src/DiaryAssignment/Diary.java
+++ OLD TASK/Assignments/src/DiaryAssignment/Diary.java
@@ -6,7 +6,7 @@ public class Diary {
     private String userName;
     private String password;
     private boolean isLocked;
-    private List<Entry> entries;
+    private List<String> entries;
 
     public Diary(String userName, String password){
         this.userName = userName;
@@ -26,21 +26,42 @@ public class Diary {
     }
 
     public void createEntry(String entryName, String password){
+        for(int index = 0; index < entries.size(); index++){
+            if(entries.get(index).equals(entryName)){
+                entries.set(index, password);
 
-
+            }
+        }
 
     }
 
     public void deleteEntry(int entryID ){
+        for( int index = 0; index < entries.size(); index++ ){
+            if( entries.get(index).equals(entryID) ){
+                entries.remove(index);
+            }
+        }
 
 
     }
 
     public int findEntryByID(int id){
-        return 0;
+        for(int index = 0; index < entries.size(); index++){
+            if( entries.get(index).equals(id) ){
+                return index;
+            }
+        }
+
+        return id;
     }
 
     public void updateEntry(int id,String name,String password){
+        for(int index = 0; index < entries.size(); index++){
+            if( entries.get(index).equals(id) ){
+                entries.set(index, name);
+
+            }
+        }
 
     }
 
