4eeaa05f0cbcd3933d455724b6378563c0595d3b
Theophilus Danladi Umar <theophilusumar27@gmail.com>
2025-07-24 03:31:11 +0100

    getters and setters

--- OLD TASK/DailyAlgoAll Lang/src/PhaseGate/StudentGrade.java
+++ OLD TASK/DailyAlgoAll Lang/src/PhaseGate/StudentGrade.java
@@ -1,14 +1,50 @@
 package PhaseGate;
 
-public class StudentGrade {
+public class StudentGrade  {
     private String studentName;
-    private String grade;
+    private List<Scores>;
     private String studentID;
+    private int position;
+    private double averageGrade;
 
-    public StudentGrade(String studentName, String grade, String studentID) {
-        this.grade = grade;
+    public StudentGrade(String studentName, String studentID, int position, double averageGrade) {
         this.studentName = studentName;
         this.studentID = studentID;
+        this.position = position;
+        this.averageGrade = averageGrade;
+    }
 
+    public String getStudentName() {
+        return studentName;
+    }
+
+    public void setStudentName(String studentName) {
+        this.studentName = studentName;
+    }
+
+    public String getStudentID() {
+        return studentID;
+    }
+
+    public void setStudentID(String studentID) {
+        this.studentID = studentID;
+    }
+
+    public int getPosition() {
+        return position;
+    }
+
+    public void setPosition(int position) {
+
+        this.position = position;
+    }
+
+    public double getAverageGrade() {
+        s
+        return averageGrade;
+    }
+
+    public void setAverageGrade(double averageGrade) {
+        this.averageGrade = averageGrade;
     }
 }
