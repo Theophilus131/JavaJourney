f93654bb3626e35ed9b3af95cd12b3f393ae1265
Theophilus Danladi Umar <theophilusumar27@gmail.com>
2025-08-10 23:34:10 +0100

    implementation of intaface method

--- OLD TASK/MyPersonalProject/src/TeLephoneTask/DeskPhone.java
+++ OLD TASK/MyPersonalProject/src/TeLephoneTask/DeskPhone.java
@@ -1,4 +1,35 @@
 package TeLephoneTask;
 
-public class DeskPhone {
+public class DeskPhone implements Telephone{
+    private String myNumber;
+    private boolean isRinging;
+
+    public DeskPhone(String myNumber){
+        this.myNumber = myNumber;
+    }
+
+    @Override
+    public void powerOn() {
+
+    }
+
+    @Override
+    public void dial(String phoneNumber) {
+
+    }
+
+    @Override
+    public void answer() {
+
+    }
+
+    @Override
+    public boolean callPhone() {
+        return false;
+    }
+
+    @Override
+    public boolean isRinging() {
+        return false;
+    }
 }
