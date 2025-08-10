6599704980a5d96508bcb7762242121379f4e1a7
Theophilus Danladi Umar <theophilusumar27@gmail.com>
2025-08-10 23:20:36 +0100

    oop telephone

---
+++ OLD TASK/MyPersonalProject/src/TeLephoneTask/Telephone.java
@@ -0,0 +1,11 @@
+package TeLephoneTask;
+
+public interface Telephone {
+
+    void powerOn();
+    void dial(String phoneNumber);
+    void answer();
+    boolean callPhone();
+    boolean isRinging();
+}
+
