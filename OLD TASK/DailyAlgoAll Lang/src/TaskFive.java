273f574210d2c1fd2d4eb6615509c627aac950aa
Theophilus Danladi Umar <theophilusumar27@gmail.com>
2025-08-25 00:27:37 +0100

    added TaskFive.java to display all names in an array in uppercase letters

---
+++ OLD TASK/DailyAlgoAll Lang/src/TaskFive.java
@@ -0,0 +1,12 @@
+
+//Write a program to display all names in an array in uppercase letters.
+
+public class TaskFive {
+    public static void main(String[] args) {
+        String[] names = {"John", "Jane", "Jill", "Joe"};
+        for (String name : names) {
+            System.out.println(name.toUpperCase());
+        }
+
+    }
+}
